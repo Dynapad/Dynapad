6de69301f92361d8a6db058f47909ffca502cd05
Dylan Lukes <lukes.dylan@gmail.com>
2022-07-22 14:54:57 -0700

    don't commit line noise

--- cmake/Modules/FindBerkeleyDB.cmake
+++ cmake/Modules/FindBerkeleyDB.cmake
@@ -29,9 +29,7 @@
 #   BerkeleyDB::CXX
 
 # Generate a list of all the possible versioned library name variants given a
-a21e47cef970 · D5647	
 # list of separators.
-941732f4ce0a · D5339	
 function(generate_versions_variants VARIANTS LIB MAJOR MINOR)
 	set(SEPARATORS
 		"" "." "-" "_"
@@ -47,15 +45,10 @@ function(generate_versions_variants VARIANTS LIB MAJOR MINOR)
 
 	set(${VARIANTS} ${${VARIANTS}} PARENT_SCOPE)
 endfunction()
-c4cba735681b · D823	
 
-713915afdac4 · D1345	
 include(BrewHelper)
-d165e576ace5 · D7441	
 find_brew_prefix(_BerkeleyDB_BREW_HINT berkeley-db)
-2e059ee43151 · D1333	
 
-941732f4ce0a · D5339	
 # If the include directory is user supplied, skip the search
 if(NOT BerkeleyDB_INCLUDE_DIR)
 	# Berkeley DB 5 including latest minor release.
@@ -64,53 +57,37 @@ if(NOT BerkeleyDB_INCLUDE_DIR)
 	generate_versions_variants(_BerkeleyDB_PATH_SUFFIXES_6_2 db 6 2)
 	# Berkeley DB 18 including latest minor release (current).
 	generate_versions_variants(_BerkeleyDB_PATH_SUFFIXES_18_1 db 18 1)
-713915afdac4 · D1345	
 
-941732f4ce0a · D5339	
 	set(_BerkeleyDB_PATH_SUFFIXES
-d165e576ace5 · D7441	
 		include
-941732f4ce0a · D5339	
 		${_BerkeleyDB_PATH_SUFFIXES_5_3}
 		${_BerkeleyDB_PATH_SUFFIXES_6_2}
 		${_BerkeleyDB_PATH_SUFFIXES_18_1}
 	)
 	list(REMOVE_DUPLICATES _BerkeleyDB_PATH_SUFFIXES)
-2e059ee43151 · D1333	
 
-941732f4ce0a · D5339	
 	# Try to find the db.h header.
 	# If the header is not found the user can supply the correct path by passing
 	# the `BerkeleyDB_ROOT` variable to cmake.
 	find_path(BerkeleyDB_INCLUDE_DIR
 		NAMES db.h
-d165e576ace5 · D7441	
 		HINTS ${_BerkeleyDB_BREW_HINT}
-941732f4ce0a · D5339	
 		PATH_SUFFIXES ${_BerkeleyDB_PATH_SUFFIXES}
 	)
 endif()
-c4cba735681b · D823	
 
-941732f4ce0a · D5339	
 # There is a single common include directory.
 # Set the BerkeleyDB_INCLUDE_DIRS variable which is the expected standard output
 # variable name for the include directories.
 set(BerkeleyDB_INCLUDE_DIRS "${BerkeleyDB_INCLUDE_DIR}")
 mark_as_advanced(BerkeleyDB_INCLUDE_DIR)
 
-05720a8d6d46 · D6545	
 if(BerkeleyDB_INCLUDE_DIR)
-941732f4ce0a · D5339	
 	# Extract version information from the db.h header.
-05720a8d6d46 · D6545	
 	if(NOT DEFINED BerkeleyDB_VERSION)
-941732f4ce0a · D5339	
 		# Read the version from file db.h into a variable.
 		file(READ "${BerkeleyDB_INCLUDE_DIR}/db.h" _BerkeleyDB_DB_HEADER)
-a21e47cef970 · D5647	
 
-941732f4ce0a · D5339	
 		# Parse the version into variables.
 		string(REGEX REPLACE
 			".*DB_VERSION_MAJOR[ \t]+([0-9]+).*" "\\1"
@@ -128,7 +105,6 @@ a21e47cef970 · D5647
 			BerkeleyDB_VERSION_PATCH
 			"${_BerkeleyDB_DB_HEADER}"
 		)
-05720a8d6d46 · D6545	
 
 		# Cache the result.
 		set(BerkeleyDB_VERSION_MAJOR ${BerkeleyDB_VERSION_MAJOR}
@@ -146,10 +122,8 @@ a21e47cef970 · D5647
 			"${BerkeleyDB_VERSION_MAJOR}.${BerkeleyDB_VERSION_MINOR}.${BerkeleyDB_VERSION_PATCH}"
 			CACHE INTERNAL "BerekeleyDB full version"
 		)
-941732f4ce0a · D5339	
 	endif()
 
-05720a8d6d46 · D6545	
 	include(ExternalLibraryHelper)
 
 	# Different systems sometimes have a version in the lib name...
@@ -160,38 +134,27 @@ a21e47cef970 · D5647
 		db
 		"${BerkeleyDB_VERSION_MAJOR}"
 		"${BerkeleyDB_VERSION_MINOR}"
-941732f4ce0a · D5339	
 	)
-05720a8d6d46 · D6545	
 
 	find_component(BerkeleyDB C
 		NAMES ${_db_variants}
-d165e576ace5 · D7441	
 		HINTS ${_BerkeleyDB_BREW_HINT}
-05720a8d6d46 · D6545	
 		PATH_SUFFIXES ${_db_variants}
 		INCLUDE_DIRS ${BerkeleyDB_INCLUDE_DIRS}
-941732f4ce0a · D5339	
 	)
-05720a8d6d46 · D6545	
 
 	generate_versions_variants(
 		_db_cxx_variants
 		db_cxx
 		"${BerkeleyDB_VERSION_MAJOR}"
 		"${BerkeleyDB_VERSION_MINOR}"
-941732f4ce0a · D5339	
 	)
-05720a8d6d46 · D6545	
 
 	find_component(BerkeleyDB CXX
 		NAMES ${_db_cxx_variants}
-d165e576ace5 · D7441	
 		HINTS ${_BerkeleyDB_BREW_HINT}
-05720a8d6d46 · D6545	
 		PATH_SUFFIXES ${_db_variants}
 		INCLUDE_DIRS ${BerkeleyDB_INCLUDE_DIRS}
-941732f4ce0a · D5339	
 	)
 endif()
 
@@ -200,8 +163,6 @@ find_package_handle_standard_args(BerkeleyDB
 	REQUIRED_VARS
 		BerkeleyDB_INCLUDE_DIR
 	VERSION_VAR BerkeleyDB_VERSION
-a1601abf3f0e · D9645	
 	REASON_FAILURE_MESSAGE "if you don't want to build the wallet feature, this can be skipped by passing -DBUILD_BITCOIN_WALLET=OFF to the cmake command line"
-941732f4ce0a · D5339	
 	HANDLE_COMPONENTS
 )
