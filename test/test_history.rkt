97aedf9839ff451209ed10ba5087a98fa06fef1e
Tom Gillespie <tgbugs@gmail.com>
2022-08-19 22:02:39 -0700

    add test_history.rkt for testing undo/redo and save interaction
    not working at the moment due to issues with missing requires,
    top-level vs module level issues, and the fact that Save-All-As breaks
    undo/redo behavior, probably missing overwriting a function somewhere

---
+++ test/test_history.rkt
@@ -0,0 +1,106 @@
+#lang racket/base
+(require racket/class
+         dynapad/events/event-state
+         (only-in dynapad/pad-state dynapad)
+         ; a newline is printing here somewhere
+         dynapad/events/event-binders-classic ; this is where our culpret is? or in menu-draw
+         dynapad/menu/menu-draw
+         "../dynapad.rkt")
+
+(provide (all-from-out
+          "../dynapad.rkt"))
+
+(require errortrace)
+(require
+ dynapad/history/logs
+ dynapad/undo-state
+ (only-in dynapad/events/draw set-Draw-object! finishDraw)
+ )
+
+(void (make-object paddraw-event-binder% dynapad event-state%))
+
+(define (make-test-objects)
+  (set-Draw-object! (ic
+    (make-object text% dynapad)
+    (id 1)
+    (anchor "nw")
+    (position -72.0f0 125.5f0 1.0f0)
+    (minsize
+     (quote
+      (0.0f0 #f)))
+    (maxsize
+     (quote
+      (-1.0f0 #f)))
+    (text "hello")
+    (font "Times")
+    (pen "#ffffff")))
+  (finishDraw dynapad)
+
+  (set-Draw-object! (ic
+    (make-object text% dynapad)
+    (id 2)
+    (anchor "nw")
+    (position -181.0f0 -26.5f0 1.0f0)
+    (minsize
+     (quote
+      (0.0f0 #f)))
+    (maxsize
+     (quote
+      (-1.0f0 #f)))
+    (text "world")
+    (font "Times")
+    (pen "#ffffff")))
+  (finishDraw dynapad)
+
+  (set-Draw-object! (ic
+    (make-object polygon% dynapad)
+    (id 3)
+    (position 102.0f0 -273.5f0 1.0f0)
+    (minsize
+     (quote
+      (0.0f0 #f)))
+    (maxsize
+     (quote
+      (-1.0f0 #f)))
+    (coords 294.0f0 -344.5f0 237.0f0 -543.5f0 -39.0f0 -446.5f0 -126.0f0 -142.5f0 149.0f0 -3.5f0 330.0f0 -76.5f0)
+    (pen "#ffffff")
+    (fill "none")))
+  (finishDraw dynapad)
+  )
+
+(module+ test
+  (require
+   (only-in dynapad/menu/menu_functions
+            Save-All-As)
+
+   #;
+   dynapad/save)
+  
+  (make-test-objects)
+  (println "setup complete")
+  (println "starting undos")
+
+  (undo)
+  (undo)
+  (undo)
+
+  (println "starting redos")
+  (redo)
+  (redo)
+  (redo)
+
+  (println "saving")
+  (Save-All-As "/tmp/dynapad-test-1-save.rktd") ; breaks undo/redo
+
+  (println "starting undos")
+  (undo)
+  (undo)
+  (undo)
+
+  (println "starting redos")
+  (redo)
+  (redo)
+  (redo)
+
+  (println "starting done")
+  )
